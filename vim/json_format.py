e180b0e60376c36c6be9c06f3b48877e8e65fac9
Farit Sadykov <sunflower@yandex-team.ru>
2019-12-27 12:39:13 +0300

    Update vim command: JSONFormat

--- vim/json_format.py
+++ vim/json_format.py
@@ -1,11 +1,15 @@
 import json
 import sys
 
-raw = sys.stdin.read()
+raw = None
 try:
-    data = json.loads(raw.encode('utf-8').decode('unicode_escape'))
-except:
-    data = json.loads(raw)
+    raw = sys.stdin.read()
+    try:
+        data = json.loads(raw.encode('utf-8').decode('unicode_escape'))
+    except:
+        data = json.loads(raw)
 
-data = json.loads(data) if isinstance(data, str) else data
-print(json.dumps(data, indent=4, ensure_ascii=False)) 
+    data = json.loads(data) if isinstance(data, str) else data
+    print(json.dumps(data, indent=4, ensure_ascii=False)) 
+except Exception as err:
+    raise Exception('Error: {}, raw: {}'.format(err, raw))
